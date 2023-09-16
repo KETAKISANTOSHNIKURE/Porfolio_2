48ed86982cd514cdcada04e4752e71d41d59b226
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-09-16 12:40:06 +0530

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -118,7 +118,7 @@ const AboutSection = () => {
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <div style={{ position: "relative", top: "-40px" }}> {/* Adjust the top value to move the image higher */}
-            <Image src="/images/hero-image1.png" width={500} height={500} />
+            <Image src="./images/hero-image1.png" width={500} height={500} />
         </div>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mt-20 mb-4 md:mt-20">About Me</h2>
