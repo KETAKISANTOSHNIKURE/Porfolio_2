1cc9c35c04c74ff709fd781b744e25ef6c89a535
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-09-17 14:20:15 +0530

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -26,27 +25,28 @@ const EmailSection = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
-    <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4"
-    >
+    <section id="contact" className="my-12 py-24">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      <div className="z-1">
-        <h5 className="text-xl font-bold text-white my-2">
-          {"Let's Connect"}
-        </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          {"I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!"}
-        </p>
-        <div className="socials flex flex-row gap-2">
-          {/* Include your SocialMediaIcons component */}
-          <SocialMediaIcons />
+      <div className="md:grid md:grid-cols-2 gap-4">
+        <div className="z-1">
+          <h5 className="text-xl font-bold text-white my-2">{"Let's Connect"}</h5>
+          <p className="text-[#ADB7BE] mb-4 max-w-md">
+            {"I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!"}
+          </p>
+          <div className="socials flex flex-row gap-2">
+            {/* Include your SocialMediaIcons component */}
+            <SocialMediaIcons />
+          </div>
+        </div>
+        <div className="hidden md:block md:col-span-1 items-center py-4 px-2 xl:gap-20 xl:px-10">
+          <Image src="./images/about-image.png" width={500} height={500} />
         </div>
       </div>
-      <div className="md:grid md:grid-cols-1 gap-4 items-center py-4 px-2 xl:gap-20 xl:px-10">
-        <Image src="./images/about-image.png" width={500} height={500} />
+      {/* Mobile-friendly image */}
+      <div className="md:hidden items-center py-4 px-2">
+        <Image src="./images/about-image.png" width={300} height={300} />
       </div>
     </section>
   );
