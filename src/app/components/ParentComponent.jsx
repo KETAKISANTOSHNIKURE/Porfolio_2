5148838831f9bdbdcf5c5be5055eb9f02719edc2
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-09-17 14:24:26 +0530

    Create ParentComponent.jsx

---
+++ src/app/components/ParentComponent.jsx
@@ -0,0 +1,17 @@
+// ParentComponent.jsx
+"use client";
+import React from "react";
+import EmailSection from "./EmailSection";
+
+const ParentComponent = () => {
+  // ...
+  return (
+    <div>
+      {/* Other components */}
+      <EmailSection />
+      {/* Other components */}
+    </div>
+  );
+};
+
+export default ParentComponent;
