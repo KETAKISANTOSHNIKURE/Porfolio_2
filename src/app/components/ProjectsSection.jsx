985ba15be853636d22be16e329711fc18ed7be0e
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-12-19 17:52:27 +0530

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,13 +7,15 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "PDetectAI APP",
-    description: "PDetectAI utilizes machine learning, specifically the K-Nearest Neighbors (KNN) algorithm, to identify early-stage Parkinson's Disease through voice input. It processes voice data via Praat software and performs calculations using Python, leveraging scikit-learn and pickle libraries.",
-    image: "./images/projects/11.png",
+    title: "Promptify",
+    description: "Promptify is a full-stack web application to discover, create and share creative prompts for the AI. It offers a seamless CRUD experience (Create, Read, Update, Delete) for prompt management and secure user authentication using NextAuth.js.",
+    image: "./images/projects/13.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/PDetect_AI",
-    previewUrl: "https://ketakisantoshnikure-pdetect-ai-app2-nmwi15.streamlit.app/",
+    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/Promptify",
+    previewUrl: "https://promptify-topaz.vercel.app/",
   },
+
+  
   {
     id: 2,
     title: "Portfolio Website",
@@ -23,18 +25,46 @@ const projectsData = [
     gitUrl: "https://github.com/KETAKISANTOSHNIKURE/Porfolio_2",
     previewUrl: "/",
   },
-  
   {
     id: 3,
-    title: "Basic Banking  Application",
-    description: "The Basic Banking System is a Android Application designed to facilitate banking transactions and provide essential banking services to users. Tech Stack : Front end- Html, Css, Javascript, Bootstrap; Back end- Php, Database, MySQL",
-    image: "./images/projects/13.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/Banking_App",
-    previewUrl: "https://www.linkedin.com/feed/update/urn:li:activity:7000008098664886272/",
+    title: "PDetectAI APP",
+    description: "PDetectAI utilizes machine learning, specifically the K-Nearest Neighbors (KNN) algorithm, to identify early-stage Parkinson's Disease through voice input. It processes voice data via Praat software and performs calculations using Python, leveraging scikit-learn and pickle libraries.",
+    image: "./images/projects/11.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/PDetect_AI",
+    previewUrl: "https://ketakisantoshnikure-pdetect-ai-app2-nmwi15.streamlit.app/",
   },
+    
+  // {
+  //   id: 3,
+  //   title: "Basic Banking  Application",
+  //   description: "The Basic Banking System is a Android Application designed to facilitate banking transactions and provide essential banking services to users. Tech Stack : Front end- Html, Css, Javascript, Bootstrap; Back end- Php, Database, MySQL",
+  //   image: "./images/projects/13.png",
+  //   tag: ["All", "Mobile"],
+  //   gitUrl: "https://github.com/KETAKISANTOSHNIKURE/Banking_App",
+  //   previewUrl: "https://www.linkedin.com/feed/update/urn:li:activity:7000008098664886272/",
+  // },
 
+ 
   {
+    id: 5,
+    title: "CupfulCalm",
+    description: "Fully responsive multipage-website created using HTML, CSS, Javascript and Bootstrap5.",
+    image: "./images/projects/12.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/CupfulCalm",
+    previewUrl: "https://ketakisantoshnikure.github.io/CupfulCalm/",
+  },
+  {
+    id: 6,
+    title: "Basic Calculator Application",
+    description: "I  develop user-friendly software applications with a focus on functionality, precision, and intuitive design. Stack Used : Html, Css, Javascript, react, Dockerfile",
+    image: "./images/projects/16.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/Calculator_Application",
+    previewUrl: "https://calculator-application-woad.vercel.app/",
+  },
+   {
     id: 4,
     title: "Todo Application",
     description: "I led a project to develop a web-based ToDo app, improving its development and deployment on Amazon Web Services (AWS). I used Docker for scalability and consistency and set up a Jenkins CI/CD pipeline for automated building, testing, and deployment. Stack Used : Django, Docker, Jenkins",
@@ -42,24 +72,6 @@ const projectsData = [
     tag: ["All", "Web"],
     gitUrl: "https://github.com/KETAKISANTOSHNIKURE/jenkins_CI-CD_Project",
     previewUrl: "/images/project/5.png",
-  },
-  {
-    id: 5,
-    title: "Super Store Sales Dashboard",
-    description: "Build a Sales Analysis, Product Analysis and Shipping Analysis Dashboard. Stack Used : Microsoft PowerBI",
-    image: "./images/projects/12.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/SuperStore_Data_Analysis_Sales_Dashboard",
-    previewUrl: "https://drive.google.com/file/d/1FFTbYVWAmBQhIkzvpAlg41ru4YI5KdxA/view?usp=drive_link",
-  },
-  {
-    id: 6,
-    title: "Basic Calculator Application",
-    description: "I  develop user-friendly software applications with a focus on functionality, precision, and intuitive design. Stack Used : Html, Css, Javascript, react, Dockerfile",
-    image: "./images/projects/16.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "https://github.com/KETAKISANTOSHNIKURE/Calculator_Application",
-    previewUrl: "https://calculator-application-woad.vercel.app/",
   },
 ];
 
