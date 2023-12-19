9a1b03223a524348f8442f95c8f95b1481285ce9
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-12-19 17:37:11 +0530

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -97,11 +97,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mt-20 mb-4 md:mt-20">About Me</h2>
           <p className="text-base text-justify lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Node.js, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+           Greetings! I'm a full-stack web developer with a flair for crafting interactive and responsive web applications. Proficient in JavaScript, React, Node.js, HTML, CSS, and Git, I'm not just a quick learner but also someone who thrives on continuous growth. A collaborative team player, I'm enthusiastic about working with others to bring exceptional applications to life. Let's build something amazing together!
             <br></br>
             Beyond coding, I'm enchanted by Bharatnatyam, where every step becomes poetry and every gesture a tale!
           </p>
