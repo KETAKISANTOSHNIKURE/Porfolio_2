8bda6f8f7db90c09c87ff76f7ee166e1413c42e6
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-12-27 16:46:02 +0530

    Create ThemeContext.js

---
+++ src/app/components/ThemeContext.js
@@ -0,0 +1,28 @@
+// ThemeContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(prevTheme => !prevTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+export { ThemeProvider, useTheme };
