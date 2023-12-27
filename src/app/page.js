7b9cca5bec6a4347506592d8e7113936d9897e3f
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-12-27 16:50:46 +0530

    Update page.js
    theme added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
@@ -6,19 +7,22 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
+import { ThemeProvider } from "./components/ThemeContext";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
-      <div class="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
+    <ThemeProvider>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Navbar />
+        <div class="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          <AchievementsSection />
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+        </div>
+        <Footer />
+      </main>
+    </ThemeProvider>
   );
 }
