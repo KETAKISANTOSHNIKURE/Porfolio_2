aa138a1539ad6274d55bbe1f7955a2ac142506c3
NIKURE KETAKI SANTOSHRAO <133886525+KETAKISANTOSHNIKURE@users.noreply.github.com>
2023-12-27 23:25:35 +0530

    Update ThemeToggle.js

--- src/app/components/ThemeToggle.js
+++ src/app/components/ThemeToggle.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { useTheme } from './ThemeContext';
 
 const ThemeToggle = () => {
-  const { toggleTheme } = useTheme();
+  const { toggleTheme } = typeof window !== 'undefined' ? useTheme() : { toggleTheme: () => {} };
 
   return (
     <button onClick={toggleTheme}>
